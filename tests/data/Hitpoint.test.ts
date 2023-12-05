613a3e3bb1080347dddcd6970ad77f4dd4665af3
Samuel Svensson <samme.s90@gmail.com>
2023-12-05 17:03:05 +0100

    Add further tests to Hitpoint

--- tests/data/Hitpoint.test.ts
+++ tests/data/Hitpoint.test.ts
@@ -4,48 +4,74 @@
 
 import { Hitpoint } from '../../src/data/Hitpoint'
 
-describe('Hitpoint', () => {
-      const total = 1
-      const regenRate = 1
-      const regenDelay = 1
+const total = 10
+const regenRate = 1
+const regenDelay = 1
 
-      test.each([-1, 0])('should be throw on %i as total', (total: number) => {
-            expect(() => new Hitpoint(total, regenRate, regenDelay)).toThrow(
-                  RangeError
-            )
+describe('Constructor', () => {
+      test('should throw on regen delay less than one', () => {
+            expect(() => new Hitpoint(total, regenRate, 0)).toThrow(RangeError)
       })
 
-      test('should be throw on negative regen rate', () => {
+      test('should throw on negative regen rate', () => {
             expect(() => new Hitpoint(total, -1, regenDelay)).toThrow(
                   RangeError
             )
       })
 
-      test('should throw on regen delay less than one', () => {
-            expect(() => new Hitpoint(total, regenRate, 0)).toThrow(RangeError)
+      test.each([-1, 0])('should throw on %i as total', (total: number) => {
+            expect(() => new Hitpoint(total, regenRate, regenDelay)).toThrow(
+                  RangeError
+            )
+      })
+})
+
+describe('Methods', () => {
+      let hitpoint: Hitpoint
+
+      beforeEach(() => {
+            hitpoint = new Hitpoint(total, regenRate, regenDelay)
       })
 
       test('should return an new object with equal values', () => {
-            const hitpoint = new Hitpoint(total, regenRate, regenDelay)
             const hitpointCopy = hitpoint.from()
             expect(hitpoint).toEqual(hitpointCopy)
             expect(hitpoint).not.toBe(hitpointCopy)
       })
 
       test('should return actual', () => {
-            const hitpoint = new Hitpoint(total, regenRate, regenDelay)
             expect(hitpoint.getActual()).toBe(total)
       })
 
       test('should reduce actual', () => {
-            const hitpoint = new Hitpoint(total, regenRate, regenDelay)
             hitpoint.reduce(1)
-            expect(hitpoint.getActual()).toBe(0)
+            expect(hitpoint.getActual()).toBe(9)
       })
 
       test('should reduce actual to zero', () => {
-            const hitpoint = new Hitpoint(total, regenRate, regenDelay)
-            hitpoint.reduce(2)
+            hitpoint.reduce(11)
             expect(hitpoint.getActual()).toBe(0)
+      })
+
+      test('should get ratio', () => {
+            hitpoint.reduce(1)
+            expect(hitpoint.getRatio()).toBe(0.9)
+      })
+
+      test('should be able to regenerate', () => {
+            hitpoint.reduce(regenRate)
+            for (let i = 0; i < regenDelay; i++) {
+                  hitpoint.regenerate()
+            }
+            expect(hitpoint.getActual()).toBe(total)
+      })
+
+      test('should not regenerate if timeout is set', () => {
+            hitpoint.reduce(regenRate)
+            hitpoint.setTimeout(regenDelay + 1)
+            for (let i = 0; i < regenDelay; i++) {
+                  hitpoint.regenerate()
+            }
+            expect(hitpoint.getActual()).toBe(total - regenRate)
       })
 })
