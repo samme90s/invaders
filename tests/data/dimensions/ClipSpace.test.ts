1e3b13571ba4b0de7902557552b70e021198f3f6
Samuel Svensson <samme.s90@gmail.com>
2024-01-03 14:33:17 +0100

    Add missing test for ClipSpace

--- tests/data/dimensions/ClipSpace.test.ts
+++ tests/data/dimensions/ClipSpace.test.ts
@@ -12,16 +12,13 @@ describe('ClipSpace', () => {
       const height = 1
       const clipSpace = new ClipSpace(width, height)
 
-      function expectHitboxToBeOutsideOn(hitbox: Hitbox) {
-            expect(clipSpace.isOutside(hitbox)).toBeTruthy
-      }
-
-      function expectHitboxToBeInsideOn(hitbox: Hitbox) {
-            expect(clipSpace.isOutside(hitbox)).toBeFalsy
-      }
+      it('should return origo', () => {
+            const expected = new Vector2(width / 2, height / 2)
+            expect(clipSpace.getOrigo()).toEqual(expected)
+      })
 
       test.each([new Vector2(width + 1, height + 1), new Vector2(-1, -1)])(
-            'should be outside when setting %d',
+            'should be outside on point',
             (point: Vector2) => {
                   expectHitboxToBeOutsideOn(
                         new Hitbox(point, direction, width, height)
@@ -29,12 +26,20 @@ describe('ClipSpace', () => {
             }
       )
 
+      function expectHitboxToBeOutsideOn(hitbox: Hitbox) {
+            expect(clipSpace.isOutside(hitbox)).toBeTruthy
+      }
+
       test.each([new Vector2(width, height), new Vector2(0, 0)])(
-            'should be inside when setting %d',
+            'should be inside on point',
             (point: Vector2) => {
                   expectHitboxToBeInsideOn(
                         new Hitbox(point, direction, width, height)
                   )
             }
       )
+
+      function expectHitboxToBeInsideOn(hitbox: Hitbox) {
+            expect(clipSpace.isOutside(hitbox)).toBeFalsy
+      }
 })
